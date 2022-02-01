15719c3ba3e06f4665d404cec5b5dfc116e068d7
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2022-02-01 13:54:00 +0300

    Initial commit

---
+++ client/src/mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "client/include/mainwindow.h"
+#include "../ui/ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
