e16f96899e77eb5bb8dc5cf7c5b72b2e48ba55f1
Jegor Popow <juicedogegor@gmail.com>
2022-02-28 10:59:31 +0300

    .

---
+++ server/src/rus.h
@@ -0,0 +1,22 @@
+#ifndef RUS_H
+#define RUS_H
+
+#include <string>
+#include <vector>
+
+class Word {
+protected:
+    std::string m_infinitive;
+    std::vector<std::string> m_forms;
+public:
+    std::string infinitive() const;
+    std::string random_form() const;
+    std::vector<std::string> const& forms() const;
+};
+
+class RusWord : public Word{
+public:
+    RusWord(std::string& corpus_line);
+};
+
+#endif
