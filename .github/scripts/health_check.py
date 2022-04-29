3f38e6cf4510703dcc2ed5e62ac35b717ec082a4
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2022-04-29 12:26:14 +0300

    Healthcheck

---
+++ .github/scripts/health_check.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import socket
+
+s = socket.socket()
+s.connect(('46.17.248.131', 1337))
+f = s.makefile(mode='rw')
+
+print("Logining...")
+f.write('{"header": { "type": "hello"},"body": {"name": "servertest123"}}\n')
+f.flush()
+f.write('{"header": { "type": "echo"},"body": {"name": "it\' working"}}\n')
+f.flush()
+assert f.readline() == '{"name":"it\' working"}\n'
+print("All works!")
