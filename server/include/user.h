5d4b61fff509b3bb1e0487a4f3afdd6e4f7215c5
Yurij Zaytsev <77232884+Hello-zoka@users.noreply.github.com>
2022-05-09 02:28:18 +0300

    delete useless DBUser class(we don't need to make smth is database if user is created)

--- server/include/user.h
+++ server/include/user.h
@@ -10,106 +10,18 @@
 
 using nlohmann::json;
 namespace FastTyping::Server {
-//    class DBUserStorage : public AbstractDatabase {
-//    public:
-//        DBUserStorage() : connect("dbname = fast_typing") {
-//            try {
-//                if (connect.is_open()) {
-//                    std::cout << "Opened database successfully: " << connect.dbname() << std::endl;
-//                } else {
-//                    std::cout << "Can't open database" << std::endl;
-//                }
-//
-//                /* Create SQL statement */
-//                sql = "CREATE TABLE IF NOT EXISTS USERS("
-//                      "ID INT PRIMARY KEY NOT NULL,"
-//                      "NAME           TEXT    NOT NULL,"
-//                      "PASSWORD           TEXT NOT NULL);";
-//
-//                /* Create a transactional object. */
-//
-//                pqxx::work W(connect);
-//                /* Execute SQL query */
-//                W.exec(sql);
-//                W.commit();
-//                std::cerr << "Table created successfully" << std::endl;
-//            } catch (const std::exception &e) {
-//                std::cerr << e.what() << std::endl;
-//            }
-//        }
-//
-//        std::string getPassword(int id) override {
-//            std::unique_lock l{mutex};
-//            pqxx::work W(connect);
-//            sql = "SELECT PASSWORD FROM USERS WHERE ID = " + std::to_string(id) + ";";
-//            pqxx::result res{W.exec(sql)};
-//            std::string passw;
-//            for (auto row: res) {
-//                passw = row["PASSWORD"].c_str();
-//            }
-//            W.commit();
-//            return passw;
-//        }
-//
-//        void setPassword(int id, const std::string &passw) override {
-//            std::unique_lock l{mutex};
-//            pqxx::work W(connect);
-//            W.exec("UPDATE USERS SET PASSWORD = '" + W.esc(passw) + "' WHERE ID = " + std::to_string(id) + ";");
-//            W.commit();
-//        }
-//
-//        std::string getName(int id) override {
-//            std::unique_lock l{mutex};
-//            pqxx::work W(connect);
-//            sql = "SELECT NAME FROM USERS WHERE ID = " + std::to_string(id) + ";";
-//            pqxx::result res{W.exec(sql)};
-//            std::string name;
-//            for (auto row: res)
-//                name = row["NAME"].c_str();
-//            W.commit();
-//            return name;
-//        }
-//
-//        int getId(const std::string &name) override {// creating new user or return existed one
-//            std::unique_lock l{mutex};
-//            pqxx::work W(connect);
-//            int id;
-//            pqxx::result find_by_name = W.exec("SELECT * FROM USERS WHERE NAME = '" + W.esc(name) + "';");
-//
-//            if (find_by_name.size() == 0) {
-//                pqxx::result full_list = W.exec("SELECT * FROM USERS;");
-//                id = full_list.size() + 1;
-//                W.exec("INSERT INTO USERS(ID, NAME, PASSWORD)\n"
-//                       "VALUES(" +
-//                       std::to_string(id) + ", '" + W.esc(name) + "', '0000');");
-//            } else {
-//                for (auto row: find_by_name)
-//                    id = row["ID"].as<int>();
-//            }
-//            W.commit();
-//            return id;
-//        }
-//
-//        bool nameExist(const std::string &name) override {
-//            std::unique_lock l{mutex};
-//            pqxx::work W(connect);
-//            pqxx::result find_by_name = W.exec("SELECT * FROM USERS WHERE NAME = '" + W.esc(name) + "';");
-//            return find_by_name.size() != 0;
-//        }
-//
-//        ~DBUserStorage() override {
-//            connect.disconnect();
-//        }
-//
-//    private:
-//        mutable std::mutex mutex;
-//        pqxx::connection connect;
-//        std::string sql;// sql query
-//    };
-
-    class DBUser {
-    public:
-        explicit DBUser(std::string &name_, Database *DB_) : userName(name_), DB(DB_), id(DB->getId(name_)), gameId(-1), wantToExit(false) {
+    struct User {
+        User() = delete;
+        explicit User(std::string &name_, Database *DB) : userName(name_), id(DB->getId(name_)), gameId(-1), wantToExit(false)  {
+        }
+        [[nodiscard]] Game *getGame() const {
+            // getGameId()
+            return currentGame.get();
+        }
+        void setGame(std::shared_ptr<Game> game) {
+            std::unique_lock l{mutex};
+            currentGame = std::move(game);
+            // setGameId(TODO)
         }
 
         [[nodiscard]] const std::string &name() const noexcept {
@@ -129,13 +41,7 @@ namespace FastTyping::Server {
         [[nodiscard]] int getGameId() const noexcept {
             return gameId;
         }
-        //        [[nodiscard]] Game *getGame() const {
-        //            return currentGame.get();
-        //        }
-        //        void setGame(std::shared_ptr<Game> game) {
-        //            std::unique_lock l{mutex};
-        //            currentGame = std::move(game);
-        //        }
+    
         void setWillToExit() noexcept {
             wantToExit = true;
         }
@@ -144,30 +50,12 @@ namespace FastTyping::Server {
         }
 
     private:
+        std::shared_ptr<Game> currentGame = nullptr;
+        mutable std::mutex mutex;
         std::string userName;
         int gameId;
         bool wantToExit;
-        Database *DB;
         int id = 0;
-    };
-
-    struct User : DBUser {
-        User() = delete;
-        explicit User(std::string &name_, Database *DB_) : DBUser(name_, DB_) {
-        }
-        [[nodiscard]] Game *getGame() const {
-            // getGameId()
-            return currentGame.get();
-        }
-        void setGame(std::shared_ptr<Game> game) {
-            std::unique_lock l{mutex};
-            currentGame = std::move(game);
-            // setGameId(TODO)
-        }
-
-    private:
-        std::shared_ptr<Game> currentGame = nullptr;
-        mutable std::mutex mutex;
     };
 }// namespace FastTyping::Server
 #endif//FASTTYPING_USER_H
