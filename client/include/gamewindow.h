ba0321fa0df41de01eef9808ce42ea377d72b52a
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2022-05-13 12:23:12 +0300

    Fixes

--- client/include/gamewindow.h
+++ client/include/gamewindow.h
@@ -25,13 +25,14 @@ private:
     Ui::GameWindow *ui;
     QPalette palette;
 
-    // std::vector<game::AbstractGameManager *> handlers;
-    // game::AbstractGameManager *main_handler;
+    //    std::vector<game::AbstractGameManager *> handlers;
+    //    game::AbstractGameManager *main_handler;
     GameManager *main_manager;
-
 signals:
     void press(QVariant key);
     void release(QVariant key);
+    // std::vector<game::AbstractGameManager *> handlers;
+    // game::AbstractGameManager *main_handler;
 
 protected:
     void keyPressEvent(QKeyEvent *event);
