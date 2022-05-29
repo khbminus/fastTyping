f2d275adc796b583c0e51673ca1a2adf737e773e
Yurij Zaytsev <77232884+Hello-zoka@users.noreply.github.com>
2022-05-29 18:36:42 +0300

    fixes

---
+++ client/src/statwindow.cpp
@@ -0,0 +1,30 @@
+#include "statwindow.h"
+#include "confirmWindow.h"
+#include "gameContextManager.h"
+#include "queryTemplates.h"
+#include "responseParse.h"
+#include "errorHandler.h"
+#include "sonicSocket.h"
+#include "ui_statwindow.h"
+#include "windowcontroller.h"
+
+StatWindow::StatWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::StatWindow) {
+    ui->setupUi(this);
+}
+
+StatWindow::~StatWindow() {
+    delete ui;
+}
+
+void StatWindow::on_ReturnButton_clicked() {
+    using client::queries::leave_query;
+    using client::web::socket;
+
+    if (confirm("Exit", "Are you really want to exit")) {
+        ContextManager::get_instance().reset_context();
+        qDebug() << "leave result: " << socket().query(leave_query());
+        auto &controller = FastTyping::WindowController::getInstance();
+        controller.setActiveWindow("MainWindow");
+    }
+}
