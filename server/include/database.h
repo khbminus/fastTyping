c7a56a925f817baf46cbc4027cb60622f0ed295b
Jegor Popow <juicedogegor@gmail.com>
2022-05-29 21:18:59 +0300

    small changes; file dictionaries prootype

--- server/include/database.h
+++ server/include/database.h
@@ -22,6 +22,7 @@ private:
     friend struct UserStorage;
     friend struct DictionariesStorage;
     friend struct ConstDictionariesStorage;
+    friend struct FileDictionariesStorage;
     friend struct MistakesStorage;
     Database();
     std::string esc(std::string const &raw);
